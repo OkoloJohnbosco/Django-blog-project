66339dd1786c0c5e83776d5942cba0e1fbafef26
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-05 01:22:30 +0100

    added a new view for individual blog post

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path("", BlogListView.as_view(), name='home')
+    path("", BlogListView.as_view(), name='home'),
+    path("", BlogDetailView.as_view(), name='home')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,3 +8,8 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_details.html"
