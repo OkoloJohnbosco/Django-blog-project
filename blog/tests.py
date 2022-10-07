538dce9b263ebe8ff7f1e439a42ce24bad9e81fc
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-07 19:35:15 +0100

    created a test class

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
+
 
 # Create your tests here.
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            title="A good title",
+            body="Nice body content",
+            author=cls.user,
+        )
