d62b7511abd542d622b931961f58cb4c16f0f4c6
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-07 20:56:00 +0100

    finished writing test for model

--- blog/models.py
+++ blog/models.py
@@ -12,4 +12,5 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("post_details", kwargs={"pk": self.pk})
+        print(reverse("post_detail", kwargs={"pk": self.pk}))
+        return reverse("post_detail", kwargs={"pk": self.pk})
--- blog/tests.py
+++ blog/tests.py
@@ -16,3 +16,10 @@ class BlogTests(TestCase):
             body="Nice body content",
             author=cls.user,
         )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "Nice body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
