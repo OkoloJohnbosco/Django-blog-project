91d2f3c135c4a1149190f23e542b336c19592455
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-10-15 22:00:27 +0100

    created the add file fxn

--- blog/tests.py
+++ blog/tests.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 from .models import Post
 
 
+def find_file():
+    return "boss"
+
+find_file()
 # Create your tests here.
 class BlogTests(TestCase):
     @classmethod
